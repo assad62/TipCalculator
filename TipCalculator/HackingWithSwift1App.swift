044a4210169ab4b8503255e9b144db6e4bd85e4a
Mohammad Assad <assad62@hotmail.com>
2023-08-23 00:37:53 -0400

    initial commit

---
+++ TipCalculator/HackingWithSwift1App.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwift1App.swift
+//  HackingWithSwift1
+//
+//  Created by Development on 22/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwift1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
